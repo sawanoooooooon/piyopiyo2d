80208534e847f5e9147bc50b2feea4c094177dfb
初コミット <sawanoyuuta@M-Yuta-Sawano-no-MacBook-Pro.local>
2018-09-24 18:25:58 +0900

    初コミット

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CameraWork();
+
+    }
+
+    void CameraWork(){
+
+        Vector3 pos = target.transform.position + new Vector3(0.0f, 1.0f, -10.0f);
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 2.0f);
+
+        }
+
+}
+
---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public float speed = 3.0f;//移動加速度
+    Rigidbody rig;
+    public float Upspeed;
+ 
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+
+
+    }
+
+
+    //プレイヤー移動
+    void Move()
+    {
+
+        //右に移動
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+        //左に移動
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+
+
+
+    }
+
+
+
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Ground" && Input.GetKey(KeyCode.Space)) 
+        {
+            rig.AddForce(transform.up * Upspeed);
+
+        }
+    }
+}
